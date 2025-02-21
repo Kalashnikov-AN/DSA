17e37d270a15cdc33f4a2a5b5756863a624d0435
Kalashnikov-AN <huumaannnnn@gmail.com>
2025-02-21 20:48:10 +0900

    comments and little fixes

--- DSA-1/Arrays, time, search/Arrays, time, search.cpp
+++ DSA-1/Arrays, time, search/Arrays, time, search.cpp
@@ -6,10 +6,10 @@ using namespace chrono;
 using namespace std;
 
 template <typename Func>
-int measure_time(Func func) {
+int measure_time(Func func) { // в параметре можно использовать std::function из <functional>
 
 	auto t0 = steady_clock::now();  // начало измерения времени
-	func();                         // выполнение функции
+	func(); // компилятор понимает, что это функция
 	auto t1 = steady_clock::now();  // конец измерения времени
 
 	auto delta = duration_cast<milliseconds>(t1 - t0);  // время в миллисекундах
@@ -20,20 +20,19 @@ int measure_time(Func func) {
 int main()
 {
 	test_is_sorted();
-	int size = 1000000000;
+	int size = 10000000;
 	const int MIN_VALUE = 0;
 	const int MAX_VALUE = 10000000;
 	int total_time = 0;
 	//int* array = createAndFillArray<int>(size, MIN_VALUE, MAX_VALUE);
 	//random_device rd;
 	//mt19937 gen(rd());  // Генератор случайных чисел
-	//uniform_int_distribution<> distr(10000, size);
+	//uniform_int_distribution<> distr(10000, size); // равномерное распределение
 	//for (int i = 0; i < 100; i++) {
 	//	
 	//	int value_to_search1 = array[distr(gen)];
-	//	total_time += measure_time([&]() {
+	//	total_time += measure_time([&]() { // фактический параметр - анонимная функция(всё, что внутри скобок)
 	//	int index = sequential_search<int>(array, size, value_to_search1);
-	//	//cout << index << "\n";
 
 	//	});
 	//}
@@ -42,14 +41,12 @@ int main()
 	int* monotonic_array = MonotonicallyIncreasingArray<int>(size, MIN_VALUE, MAX_VALUE);
 	random_device rd;
 	mt19937 gen(rd());  // Генератор случайных чисел
-	uniform_int_distribution<> distr(10000, size);
-	//uniform_int_distribution<> distr(10000, size);
+	uniform_int_distribution<> distr(10000, size); // равномерное распределение
+	//uniform_int_distribution<> distr(10000, size); // равномерное распределение
 	for (int i = 0; i < 100; i++) {
 		int value_to_search1 = monotonic_array[distr(gen)];
-		total_time += measure_time([&]() {
+		total_time += measure_time([&]() { // фактический параметр - анонимная функция(всё, что внутри скобок)
 			int index = sequential_search<int>(monotonic_array, size, value_to_search1);
-			//cout << index << "\n";
-
 			});
 	}
 	cout << "average time in ms for monotonically increasing array = " << total_time / 100.0;
--- DSA-1/Arrays, time, search/arrays.h
+++ DSA-1/Arrays, time, search/arrays.h
@@ -8,7 +8,7 @@ template <typename T>
 T* createAndFillArray(size_t size, T min_value, T max_value) {
     random_device rd;
     mt19937 gen(rd());  // Генератор случайных чисел
-    uniform_int_distribution<> distr(min_value, max_value);
+    uniform_int_distribution<> distr(min_value, max_value); // равномерное распределение
 
     T* array = new T[size];  // Создание массива
 
@@ -24,7 +24,7 @@ template <typename T>
 T* MonotonicallyIncreasingArray(size_t size, T min_value, T max_value) {
     random_device rd;
     mt19937 gen(rd());  // Генератор случайных чисел
-    uniform_int_distribution<> distr(min_value, max_value);
+    uniform_int_distribution<> distr(min_value, max_value); // равномерное распределение
     T* array = new T[size];  // Создание массива
     array[0] = distr(gen);
     for (size_t i = 1; i < size; ++i) {
@@ -35,7 +35,7 @@ T* MonotonicallyIncreasingArray(size_t size, T min_value, T max_value) {
 }
 
 template <typename T>
-void array_to_file(T* array, size_t size, const string filename) {
+void array_to_file(T* array, size_t size, const string& filename) {
     ofstream f(filename);
     if (f.is_open()) {
         for (size_t i = 0; i < size; ++i) {
@@ -63,7 +63,7 @@ int sequential_search(T* array, size_t size, T value) {
             
         }
     }
-    return -1;  // Если элемент не найден
+    return size;  // Если элемент не найден
 }
 
 void test_is_sorted();
