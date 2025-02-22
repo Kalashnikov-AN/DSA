ab5cd086804b8f445f158b983f41c8d50a86fc67
Kalashnikov-AN <huumaannnnn@gmail.com>
2025-02-22 23:37:08 +0900

    stack overflow

---
+++ DSA-1/Stack Overflow/Stack Overflow.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+long long counter; // Счётчик вызовов рекурсии
+
+/// Функция для измерения глубины стека
+void recursiveFunction() {
+    ++counter; // Инкрементируем счётчик вызовов рекурсии
+    cout << "Stack depth: " << counter << endl;
+
+    // Вызываем функцию рекурсивно, пока не произойдет переполнение стека
+    recursiveFunction();
+
+}
+
+int main() {
+        recursiveFunction(); // Вызываем функцию рекурсивно, пока не произойдет переполнение стека
+    
+}
