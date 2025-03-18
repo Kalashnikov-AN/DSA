39f27db3f75717e9e37d209b3231e6a498e54eba
Kalashnikov-AN <huumaannnnn@gmail.com>
2025-03-19 00:52:28 +0900

    push recursion task

---
+++ DSA-1/GCD/GCD.cpp
@@ -0,0 +1,18 @@
+﻿// Автор: Калашников А.Н.
+
+#include <iostream>
+#include <cassert>
+#include <locaLe>
+#include "calculate.h"
+using namespace std;
+
+// Задание: 8. Найти НОД (наибольший общий делитель) двух натуральных чисел.
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    testGCD(); // Запуск тестов для функции gcd
+    cout << gcd(18, 27) << endl;
+    cout << gcd(123, 456) << endl;
+    cout << gcd(128, 1024) << endl;
+}
