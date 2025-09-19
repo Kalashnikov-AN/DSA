59741fdc60c21bed0b192ac02c5a78df0f7edfa4
Kalashnikov-AN <huumaannnnn@gmail.com>
2025-09-19 11:53:39 +0900

    different functions for tree node

--- DSA-2/ClassTreeNode/ClassTreeNode.cpp
+++ DSA-2/ClassTreeNode/ClassTreeNode.cpp
@@ -1,6 +1,7 @@
 ﻿//Автор: Калашников А.Н.
 #include <iostream>
 #include "TreeNode.h"
+#include "TreeNodeFunctions.h"
 using namespace std;
 
 int main()
@@ -13,32 +14,17 @@ int main()
 	root->right = new TreeNode<int>(12);
 	root->right->left = new TreeNode<int>(10);
 	root->right->right = new TreeNode<int>(14);
-	
+	print_tree(root, 10);
+	preorder_print<int>(root);
 	// Просматриваем содержимое двух узлов дерева в качестве примера 
 	cout << ("Корень дерева = ") << root->value;
 	cout << endl; 
 	cout << ("Корень дерева -> направо -> направо =  ") << root->right->right->value;
 	cout << endl;
-	int i = 0;
-
-	// Удаляем все узлы дерева
-	while (root) {
-		if (root->left) {
-			TreeNode<int>* tmp = root->left;   // сохраняем ссылку на левого потомка
-			root->left = tmp->right;           // переносим правую ветку наверх
-			tmp->right = root;                 // делаем временный цикл
-			root = tmp;                        // спускаемся вниз
-		}
-		else {
-			TreeNode<int>* tmp = root;         // текущий узел
-			root = root->right;                // переходим направо
-			cout << tmp->value << endl;
-			delete tmp;                        // удаляем узел
-			
-			i++;
-			cout << i << endl;
-		}
-	}
+	cout << count_nodes<int>(root);
+	cout << endl;
+	cout << depth(root);
+	delete_tree(root);
 
 	/* 
 	Дерево является строгим, т.к. у каждого узла либо 0, либо 2 потомка.
